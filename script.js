10df6e2bd09671d829668dda98ecc2d7f0e89fc3
ahmed <ahm.gmal@gmail.com>
2021-03-29 03:15:44 -0400

    Genre switch

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-let h2 = document.createElement('h2')
-h2.textContent = "This content added by JavaScript"
-document.querySelector('body').appendChild(h2)
+const btnSwitch = document.querySelector(".switch");
+const mafia = document.getElementById("lstmafia");
+const romcom = document.getElementById("lstromcom");
+
+btnSwitch.addEventListener("click", switchGenre());
+
+function switchGenre(){
+    
+    if(btnSwitch.innerHTML === "Mafia"){
+        showMafia();
+        hideRomCom();
+    }
+    else{
+        showRomCom();
+        hideMafia();
+    }
+}
+
+function showMafia(){
+    mafia.classList.remove("hide");
+    btnSwitch.textContent = "Mafia"
+}
+
+function hideMafia(){
+   mafia.classList.add("hide")
+}
+
+function showRomCom(){
+    romcom.classList.remove("hide");
+    btnSwitch.textContent = "RomCom"
+}
+
+function hideRomCom(){
+   romcom.classList.add("hide")
+}
